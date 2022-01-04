8802cfbd47d208d580efaf32746afb2c12f3e9f4
Tuuvee <tuukka.tahtinen@hotmail.fi>
2022-01-04 15:30:42 +0200

    Basic layout and TODO-List added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import TodoList from './Components/TodoList.js';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		<div className="WeatherContainer">
+		<p>yeah</p> {/*Weather display goes here when i get to it*/}
+		</div>
+		
+		<div className="ContentContainer">
+		<p>yeah</p>{/*Other API Content goes here*/}
+		</div>
+		
+		<div className="TodoContainer">
+		<TodoList />
+		</div>
     </div>
   );
 }
